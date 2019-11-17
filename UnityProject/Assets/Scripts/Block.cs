89360593bceadcbf08dc19dc0a4fbb27a62de4f6
nagaohiroki <testplayers0226@gmail.com>
2019-11-17 15:59:41 +0900

    tile 配置

---
+++ UnityProject/Assets/Scripts/Block.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+public class Block
+{
+	public enum Status
+	{
+		Empty,
+		Put
+	}
+	public Status mStatus = Status.Empty;
+	public Vector3 mPos;
+}
+public class BlockTable
+{
+	Block[,] mBlock;
+	public BlockTable(Vector2Int inSize)
+	{
+		var size = inSize * 2;
+		mBlock = new Block[size.y, size.x];
+		for(int y = 0; y < mBlock.GetLength(0); ++y)
+		{
+			for(int x = 0; x < mBlock.GetLength(1); ++x)
+			{
+				mBlock[y, x] = new Block();
+			}
+		}
+	}
+	public Block GetBlock(Vector2Int inPos)
+	{
+		int numX = mBlock.GetLength(1);
+		int numY = mBlock.GetLength(0);
+		int x =  numX / 2 + inPos.x;
+		int y =  numY / 2 + inPos.y;
+		if(x < 0 || x >= numX || y < 0 || y >= numY)
+		{
+			return null;
+		}
+		return mBlock[y, x];
+	}
+}
---
+++ UnityProject/Assets/Scripts/Pawn.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+public class Pawn : MonoBehaviour
+{
+	[SerializeField]
+	Text mText = null;
+	[SerializeField]
+	NavMeshAgent mAgent = null;
+	[SerializeField]
+	float mSpeed = 0.0f;
+	[SerializeField]
+	GameObject mCube = null;
+	const float mScale = 1.0f;
+	BlockTable mBlockTable = new BlockTable(new Vector2Int(10, 10));
+	static Vector2Int GetIndex(Vector3 inPos)
+	{
+		var index = Vector2Int.zero;
+		index.x = Mathf.FloorToInt(inPos.x / mScale);
+		index.y = Mathf.FloorToInt(inPos.z / mScale);
+		return index;
+	}
+	static Vector3 GetPos(Vector2Int inIndex)
+	{
+		var pos = Vector3.zero;
+		pos.x = inIndex.x * mScale;
+		pos.z = inIndex.y * mScale;
+		return pos;
+	}
+	void Move()
+	{
+		var offset = Vector3.zero;
+		offset.x = Input.GetAxis("Horizontal");
+		offset.z = Input.GetAxis("Vertical");
+		mAgent.Move(offset * mSpeed * Time.deltaTime);
+		Camera.main.transform.parent.position = transform.position;
+	}
+	Vector3 GetCursorPos()
+	{
+		// カーソル位置
+		var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if(Physics.Raycast(ray, out hit, Mathf.Infinity, Physics.DefaultRaycastLayers, QueryTriggerInteraction.Ignore))
+		{
+			return hit.point;
+		}
+		return Vector3.zero;
+	}
+	void Shot()
+	{
+		var index = GetIndex(GetCursorPos());
+		var block = mBlockTable.GetBlock(index);
+		if(block == null)
+		{
+			return;
+		}
+		if(block.mStatus != Block.Status.Empty)
+		{
+			return;
+		}
+		var offset = new Vector3(mScale, 0.0f, mScale) * 0.5f;
+		var pos = GetPos(index) + offset;
+		mCube.transform.position = pos;
+		if(Input.GetButtonDown("Fire1"))
+		{
+			block.mStatus = Block.Status.Put;
+			var obj = Instantiate(mCube, pos, Quaternion.identity);
+			obj.SetActive(true);
+			obj.GetComponent<Collider>().isTrigger = false;
+			obj.transform.localScale = Vector3.one * mScale;
+			var render = obj.GetComponent<Renderer>();
+			render.material.color = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), 1.0f);
+			obj.GetComponent<NavMeshObstacle>().enabled = true;
+		}
+	}
+	void Update()
+	{
+		Move();
+		Shot();
+		DebugLog();
+	}
+	void DebugLog()
+	{
+		var index = GetIndex(transform.position);
+		var cursor = GetIndex(GetCursorPos());
+		mText.text = string.Format("index: {0}, cursor: {1}", index, cursor);
+	}
+}
